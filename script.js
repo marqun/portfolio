3b8aea43934be43d06f16549f7c0ec55869d11da
marqun <marqun@users.noreply.github.com>
2019-02-16 21:58:13 +0100

    add buttons to move up/down and make display +1000px

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("javascript test")
+console.log("javascript test");
+
+
+function myFunction2(){
+
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+}
+
+function myFunction(){
+
+    window.scrollTo(0,document.body.scrollHeight);
+
+}
